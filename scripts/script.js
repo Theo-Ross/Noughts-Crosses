407e01fbdc0dc1e463a85dee6f8f7754287f61d5
Theo-Ross <TheoRoss99@icloud.com>
2022-11-28 23:44:56 +0000

    theme switch working

--- scripts/script.js
+++ scripts/script.js
@@ -18,18 +18,41 @@ const resetButton = document.querySelector(".reset")
 let player1Score = document.getElementById("player1Score");
 let player2Score = document.getElementById("player2Score");
 
+const body = document.querySelector("body")
+const themeSwitch = document.getElementById("themeSwitch")
 
+const Ochange = document.querySelector(".Ochange")
+const Xchange = document.querySelector(".Xchange")
 
 let previousSymbol = ""
 let symbol = ""
 
+// theme swticher
+const switchThemes = (event) => {
+    if (body.classList.contains("retro")) {
+        body.classList.remove("retro");
+        body.classList.add("light");
+    } else if (body.classList.contains("light")) {
+        body.classList.remove("light");
+        body.classList.add("neon");
+    } else if (body.classList.contains("neon")) {
+        body.classList.remove("neon");
+        body.classList.add("retro");
+    }
+
+    
+}
+
+themeSwitch.addEventListener("click", switchThemes)
+
+//////
+
 const playMove = (event) => {
-    console.log(event.target.innerHTML)
     if ( event.target.innerHTML ===  "O") {
         window.alert("error")
         // turn this into one statement
     } else if ( event.target.innerHTML === "X") {
-
+        window.alert("error")
     }
     else {
         // maybe simplify this line
@@ -116,10 +139,10 @@ const playerWinner = document.querySelector(".buttons_winner")
 // color function 
 const changeColor = (event ) => {
     if (event.target.innerHTML === "O") {
-        event.target.style.color = "red";
+        event.target.classList.add("Ochange");
         }
         else if (event.target.innerHTML === "X") {
-            event.target.style.color = "blue";
+            event.target.classList.add("Xchange");
         }
 }
 
@@ -151,45 +174,50 @@ restartButton.addEventListener ("click", restartGame)
 resetButton.addEventListener("click", resetGame )
 
 // turn this into one function to execute all three
-block1.addEventListener("click", playMove)
-block1.addEventListener("click", checkForWin)
-block1.addEventListener("click", changeColor)
 
-block2.addEventListener("click", playMove)
-block2.addEventListener("click", checkForWin)
-block2.addEventListener("click", changeColor)
+const clickFunction = (event) => {
+    playMove, checkForWin, changeColor
+}
 
+block1.addEventListener("click", playMove);
+block1.addEventListener("click", checkForWin);
+block1.addEventListener("click", changeColor);
 
-block3.addEventListener("click", playMove)
-block3.addEventListener("click", checkForWin)
-block3.addEventListener("click", changeColor)
-
-
-block4.addEventListener("click", playMove)
-block4.addEventListener("click", checkForWin)
-block4.addEventListener("click", changeColor)
+block2.addEventListener("click", playMove);
+block2.addEventListener("click", checkForWin);
+block2.addEventListener("click", changeColor);
 
 
-block5.addEventListener("click", playMove)
-block5.addEventListener("click", checkForWin)
-block5.addEventListener("click", changeColor)
+block3.addEventListener("click", playMove);
+block3.addEventListener("click", checkForWin);
+block3.addEventListener("click", changeColor);
 
 
-block6.addEventListener("click", playMove)
-block6.addEventListener("click", checkForWin)
-block6.addEventListener("click", changeColor)
+block4.addEventListener("click", playMove);
+block4.addEventListener("click", checkForWin);
+block4.addEventListener("click", changeColor);
 
 
-block7.addEventListener("click", playMove)
-block7.addEventListener("click", checkForWin)
-block7.addEventListener("click", changeColor)
+block5.addEventListener("click", playMove);
+block5.addEventListener("click", checkForWin);
+block5.addEventListener("click", changeColor);
 
 
-block8.addEventListener("click", playMove)
+block6.addEventListener("click", playMove);
+block6.addEventListener("click", checkForWin);
+block6.addEventListener("click", changeColor);
+
+
+block7.addEventListener("click", playMove);
+block7.addEventListener("click", checkForWin);
+block7.addEventListener("click", changeColor);
+
+
+block8.addEventListener("click", playMove);
 block8.addEventListener("click", checkForWin)
-block8.addEventListener("click", changeColor)
+block8.addEventListener("click", changeColor);
 
 
-block9.addEventListener("click", playMove)
+block9.addEventListener("click", playMove);
 block9.addEventListener("click", checkForWin);
-block9.addEventListener("click", changeColor)
+block9.addEventListener("click", changeColor);
